e2b8e5909bf7018b5cdfb93aa3cd90087e074a07
Jing <jingkcheng@hotmail.com>
2019-06-09 22:32:52 -0400

    Front end push

---
+++ index.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+let express = require('express');
+
+app.use('/static', express.static('static'))
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+}); 
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
